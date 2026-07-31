1933a9267bcba2fdeb5db4d8506ced09f8abd8a5
zfjrblxx <zfjrblxx@gmail.com>
2026-07-31 22:37:43 +0700

    Update save.js

--- js/save.js
+++ js/save.js
@@ -1,1 +1,32 @@
 
+function saveGame(){
+
+localStorage.setItem(
+
+"devilHunter",
+
+JSON.stringify({
+
+player:Player,
+
+game:Game
+
+})
+
+);
+
+}
+
+function loadGame(){
+
+const data=localStorage.getItem("devilHunter");
+
+if(!data)return;
+
+const save=JSON.parse(data);
+
+Object.assign(Player,save.player);
+
+Object.assign(Game,save.game);
+
+}
