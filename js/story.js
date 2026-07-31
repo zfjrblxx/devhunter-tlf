bd700f0bb01d4475edfe1471368919fabecb1e3d
zfjrblxx <zfjrblxx@gmail.com>
2026-07-31 22:43:34 +0700

    Update story.js

--- js/story.js
+++ js/story.js
@@ -41,3 +41,45 @@ Continue
 typeNextLine();
 
 }
+
+
+
+
+function typeNextLine(){
+
+if(currentLine>=introText.length){
+
+document.getElementById("nextStory")
+.classList.remove("hidden");
+
+return;
+
+}
+
+const text=document.getElementById("storyText");
+
+const p=document.createElement("p");
+
+text.appendChild(p);
+
+let i=0;
+
+const timer=setInterval(()=>{
+
+p.textContent+=introText[currentLine][i]||"";
+
+i++;
+
+if(i>introText[currentLine].length){
+
+clearInterval(timer);
+
+currentLine++;
+
+setTimeout(typeNextLine,400);
+
+}
+
+},25);
+
+}
